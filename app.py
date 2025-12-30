709911d67226e1d1252d7667b4f5b9aab5528285
Dede Ahmad Maolana <155211526+dee-aah@users.noreply.github.com>
2025-12-30 20:57:48 +0700

    .

--- app.py
+++ app.py
@@ -160,8 +160,8 @@ tab1, tab2 = st.tabs([" Live Monitor", " Deteksi Manual"])
 
 with tab1:
     with st.expander("⚙️ Pengaturan Live"):
-        api_key = st.secrets.get("YOUTUBE_API_KEY", "")
-        # api_key = st.text_input("YouTube API Key", type="password")
+        # api_key = st.secrets.get("YOUTUBE_API_KEY", "")
+        api_key = st.text_input("YouTube API Key", type="password")
         video_id = st.text_input("Masukkan YouTube Video ID" ,placeholder="Contoh: dQw4w9WgXcQ")
 
         # c1, c2 = st.columns(2)
